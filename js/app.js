e11086a2b85b0b0bb531d19382e3d3aaea3f4254
Chris Kelley <ckemail2@gmail.com>
2019-08-10 19:53:06 -0400

    add eventlistener functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-/*
- * Create a list that holds all of your cards
- */
 
+
+// Create a list that holds all of your cards
+/*
+
+const cardDeck = ['fa-diamond', 'fa-diamond',
+               'fa-paper-plane', 'fa-paper-plane',
+               'fa-anchor', 'fa-anchor',
+               'fa-bolt', 'fa-bolt',
+               'fa-cube', 'fa-cube',
+               'fa-leaf', 'fa-leaf',
+               'fa-bicycle', 'fa-bicycle',
+               'fa-bomb', 'fa-bomb'
+             ];
+*/
+
+let openCards = [];
+let moveCounter = 0;
+let matchCounter = 1;
 
 /*
  * Display the cards on the page
@@ -9,6 +24,10 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +44,83 @@ function shuffle(array) {
     return array;
 }
 
+// Diplay the card's symbol
+function showCard(card) {
+    card.classList.add('open', 'show');
+ }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+// Add the current card to list of open cards
+function addToOpenCards(card) {
+    openCards.push(card);
+}
+
+// Check to see if the cards match
+function checkMatch() {
+    if(openCards[0].firstElementChild.className == openCards[1].firstElementChild.className){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// cards match
+function matchCards() {
+    openCards[0].classList.add('match');
+    openCards[0].classList.remove('open', 'show');
+    openCards[1].classList.add('match');
+    openCards[1].classList.remove('open', 'show');
+    openCards = [];
+}
+
+// cards do not match
+function hideCards() {
+    setTimeout(function() {
+      openCards[0].classList.remove('open', 'show');
+      openCards[1].classList.remove('open', 'show');
+      openCards = [];
+    }, 1500);
+}
+
+// increment the move counter and display it on the page
+function incrementMoveCounter() {
+    moveCounter++;
+    // update move counter on page, display it on the page
+}
+
+// Increment the match counter
+function incrementMatchCounter() {
+    matchCounter++;
+}
+
+// All cards have matched - display a message with the final score}
+function endGame(){
+    console.log('game over')
+    console.log('matchCounter')
+}
+
+const cards = document.getElementsByClassName('card');
+
+// EventListener for cards
+for (const card of cards) {
+    card.addEventListener('click', function() {
+        if(!card.classList.contains('match')){ // Ignore cards that are already matched
+            showCard(card);
+            addToOpenCards(card);
+
+            if(openCards.length == 2){ // If two cards have been selected
+                if(checkMatch()){
+                    matchCards();
+                } else {
+                    hideCards();
+                }
+            }
+
+            incrementMoveCounter();
+            incrementMatchCounter();
+
+            if(matchCounter == 8){
+                endGame();
+            }
+        }
+    });
+}
